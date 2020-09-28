99a1d40e7efd9c30a561611ebb0644982f1214ce
Aghora7 <ukikieran@gmail.com>
2020-09-28 17:45:42 +0000

    Init

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter olive olivelite pine,$(TARGET_DEVICE)),)
+ifneq ($(filter olive olivelite olivewood pine,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
